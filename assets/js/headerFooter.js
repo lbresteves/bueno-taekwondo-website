76db1e4e8e56ceb979e222e498238175708c56ee
LEsteves <leticia.braga999@gmail.com>
2024-02-20 22:10:22 -0300

    Lançamento página inicial

--- assets/js/headerFooter.js
+++ assets/js/headerFooter.js
@@ -14,9 +14,9 @@ class SpecialHeader extends HTMLElement {
                                 <!-- ***** Menu Start ***** -->
                                 <ul class="nav">
                                     <li><a href="index.html">Home</a></li>
-                                    <li><a href="about.html">Sobre</a></li>
-                                    <li><a href="deals.html">Fotos</a></li>
-                                    <li><a href="reservation.html">Marcar Aula</a></li>
+                                    <li><a href="dev_page.html">Sobre</a></li>
+                                    <li><a href="dev_page.html">Horários</a></li>
+                                    <li><a href="dev_page.html">Fotos</a></li>
                                 </ul>   
                                 <a class='menu-trigger'>
                                     <span>Menu</span>
